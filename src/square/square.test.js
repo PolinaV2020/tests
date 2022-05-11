1fe6ce902a30bda0507fe90120ca8dbbbef4115d
Palina.Ulashchyk <palina.ulashchyk@itechart-group.com>
2022-05-11 13:35:13 +0300

    add: mocked data testing

--- src/square/square.test.js
+++ src/square/square.test.js
@@ -1,9 +1,18 @@
 const square = require("./square");
 
 describe("Square function", () => {
-  test("Correct value", () => {
+  test("Pow method is called once", () => {
     const spyMathPow = jest.spyOn(Math, "pow");
     square(2);
     expect(spyMathPow).toBeCalledTimes(1);
   });
+  test("Pow method isn't called", () => {
+    const spyMathPow = jest.spyOn(Math, "pow");
+    square(1);
+    expect(spyMathPow).toBeCalledTimes(0);
+  });
+
+  afterEach( () => {
+    jest.clearAllMocks();
+  })
 });
